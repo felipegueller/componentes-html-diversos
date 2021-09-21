ce16f798d9d80ce7474d435b3f59cd714c596670
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2021-09-21 18:27:52 -0300

    feat: finalizing the mobile menu

--- menu-mobile-origamid/index.js
+++ menu-mobile-origamid/index.js
@@ -1,12 +1,24 @@
 const btnMobile = document.getElementById('btn-mobile')
 
-function toggleMenu() {
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('nav')
 
     // Adicionando uma classe CSS, demoninada 'active'
     // toggle -> adicione caso tenha, remova caso não tenha.
     nav.classList.toggle('active')
+
+    // Adicionando a mudança do aria-expanded para parte de acessibilidade (Arias)
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active)
+
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir menu')
+    }
 }
 
 // Quando o evento do click for identificado, a função toggleMenu é chamada
 btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
