418ed5b045b7f41cc25ca1e87be8540f47dafedf
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2021-10-13 22:18:16 -0300

    feat: stylizing the background of progress bar by javascript

--- inputs/input-range/script.js
+++ inputs/input-range/script.js
@@ -7,8 +7,16 @@ inputSlider.oninput = () => {
     slideValue.textContent = value
     slideValue.style.left = (value/2) + "%"
     slideValue.classList.add('show')
+    changeProgressBarColor(inputSlider)
 }
 
 inputSlider.onchange = () => {
     slideValue.classList.remove('show')
+    changeProgressBarColor(inputSlider)
+}
+
+function changeProgressBarColor(input) {
+    const percentage = ((input.value - input.min) / (input.max - input.min) * 100 )
+    const color = `linear-gradient(90deg, #664AFF ${percentage}%, #DDD ${percentage}%)`
+    input.style.background = color
 }
