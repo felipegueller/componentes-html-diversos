8c10a92c432d781e03f60e0660371363c69b8842
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2021-12-28 11:01:03 -0300

    feat: adding the logic to show and select the suggestions

--- search-box-autocomplete/scripts/script.js
+++ search-box-autocomplete/scripts/script.js
@@ -1,5 +1,5 @@
 const searchWrapper = document.querySelector('.search-input')
-const inputBox = document.querySelector('#searcher')
+const inputBox = document.querySelector('input#searcher')
 const suggestionBox = document.querySelector('.autocom-box')
 
 inputBox.onkeyup = event => {
@@ -12,9 +12,36 @@ inputBox.onkeyup = event => {
     })
 
     // transforming to li tag
-    matchedSuggestions = matchedSuggestions.map(data => '<li>' + data + '</li>')
-    console.log(matchedSuggestions)
+    matchedSuggestions = matchedSuggestions.map(data => {
+      return '<li>' + data + '</li>'
+    })
+
+    showSuggestions(matchedSuggestions)
+    searchWrapper.classList.add('active')
+
+    const allListItem = Array.from(document.querySelectorAll('.autocom-box li'))
+    allListItem.forEach(item => {
+      item.onclick = () => selectedListItem(item)
+    })
+  } else {
+    searchWrapper.classList.remove('active')
   }
 }
 
-// https://www.youtube.com/watch?v=QxMBHi_ZiT8
+function selectedListItem(item) {
+  // passing the content of the selected item to inputField
+  inputBox.value = item.textContent
+  searchWrapper.classList.remove('active')
+}
+
+function showSuggestions(suggestions) {
+  let suggestionsList, userValue
+
+  if (suggestions.length <= 0) {
+    userValue = inputBox.value
+    suggestionsList = '<li>' + userValue + '</li>'
+  } else {
+    suggestionsList = suggestions.join('')
+  }
+  suggestionBox.innerHTML = suggestionsList
+}
