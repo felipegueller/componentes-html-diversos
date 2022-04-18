0460399d4a5f79e0f1c23c48619c65e4a0bbdd1a
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-04-18 11:44:13 -0300

    feat: adding the logic to work the suggestions box

--- tables/table-with-search-inputs-inside/scripts/script.js
+++ tables/table-with-search-inputs-inside/scripts/script.js
@@ -1,6 +1,13 @@
+// table constants
 const url = 'https://jsonplaceholder.typicode.com/users'
 const tableContainer = document.querySelector('.table-container')
 const tbody = document.querySelector('table[data-js="table"] tbody')
+
+// suggestion autocomplete box constants
+const inputSearch = document.querySelectorAll('input[data-js="input-search"]')
+const suggestionsLists = document.querySelectorAll(
+  'ul[data-js="suggestions-list"]'
+)
 
 const getUsers = async () => {
   try {
@@ -9,6 +16,44 @@ const getUsers = async () => {
   } catch (error) {
     console.log(error)
   }
+}
+
+const showSuggestions = (suggestions, input) => {
+  if (suggestions.length <= 0) suggestions = `<li>${input.value}</li>`
+
+  const inputWrapper = input.parentNode
+  const suggestionsList = inputWrapper.querySelector('.suggestions-box ul')
+  suggestionsList.innerHTML = suggestions
+}
+
+const findMatchedSuggestions = async event => {
+  const input = event.target
+  const parentInputWrapper = input.parentNode
+
+  if (!input.value) {
+    parentInputWrapper.classList.remove('active')
+    return
+  }
+
+  let matchedSuggestions = []
+
+  const userData = await getUsers()
+  const apiData = userData.map(user => {
+    const key = input.name
+    return user[key]
+  })
+
+  matchedSuggestions = apiData.filter(suggestion => {
+    return suggestion.toLowerCase().startsWith(input.value.toLowerCase())
+  })
+
+  matchedSuggestions = matchedSuggestions.reduce((acc, suggestion) => {
+    return `${acc}<li>${suggestion}</li>`
+  }, '')
+
+  showSuggestions(matchedSuggestions, input)
+
+  parentInputWrapper.classList.add('active')
 }
 
 const addDataOnTable = async () => {
@@ -36,6 +81,32 @@ const addDataOnTable = async () => {
   tbody.innerHTML += tableRows
 }
 
+const puttingSuggestionOnInput = event => {
+  event.stopPropagation()
+
+  const element = event.target
+  const tagName = element.tagName
+
+  if (tagName !== 'LI') return
+
+  const inputWrapper = element.closest('.input-wrapper')
+  const input = inputWrapper.querySelector('input')
+
+  input.value = element.textContent
+  inputWrapper.classList.remove('active')
+}
+
+suggestionsLists.forEach(suggestionList => {
+  suggestionList.addEventListener('click', puttingSuggestionOnInput)
+})
+
+inputSearch.forEach(input => {
+  input.addEventListener('input', findMatchedSuggestions)
+})
+
+window.addEventListener('DOMContentLoaded', addDataOnTable)
+
+// Code to fix the table head
 // const tableFixHead = event => {
 //   const tableContainer = event.target,
 //     scrollTop = tableContainer.scrollTop,
@@ -49,4 +120,3 @@ const addDataOnTable = async () => {
 // }
 
 // tableContainer.addEventListener('scroll', tableFixHead)
-window.addEventListener('DOMContentLoaded', addDataOnTable)
