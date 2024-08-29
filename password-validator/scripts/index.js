8e2834255607c62047a8ab49149df51a6d398386
Felipe Gueller <felipe.gueller@gmail.com>
2024-08-29 17:03:56 -0300

    :sparkles: feat: add login form

--- password-validator/scripts/index.js
+++ password-validator/scripts/index.js
@@ -1,35 +1,9 @@
+// 1 - Seleciona os elementos em tela
 const input = document.querySelector('#password')
+const iconBox = document.querySelector('.icon-box')
 const icon = document.querySelector('#icon')
-// const passwordValidator = require('password-validator')
 
-// input.oninput = event => {
-//   const password = event.target.value
-
-//   const schema = new PasswordValidator()
-
-//   schema
-//     .is()
-//     .min(10)
-//     .is()
-//     .max(100)
-//     .has()
-//     .uppercase(1)
-//     .has()
-//     .lowercase()
-//     .has()
-//     .digits(2)
-//     .has()
-//     .not()
-//     .spaces()
-//     .is()
-//     .not()
-//     .oneOf(['Passw0rd', 'Password123'])
-
-//   console.log(schema.validate(password, { details: true }))
-// }
-
-icon.onclick = () => showPassword()
-
+// 2 - Cria a função que mostra a senha
 function showPassword() {
   input.type === 'password' ? (input.type = 'text') : (input.type = 'password')
   toggleIcon()
@@ -39,3 +13,6 @@ function toggleIcon() {
   icon.classList.toggle('fa-eye')
   icon.classList.toggle('fa-eye-slash')
 }
+
+// 3 - Adiciona o evento de clique no ícone
+iconBox.addEventListener('click', showPassword)
